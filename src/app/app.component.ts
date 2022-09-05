7efc0b14714fbed456bfbd7e08d1f650455e2ffb
Матвей Калинин <m_kalinin@dev-city.local>
2022-09-05 13:44:28 +0300

    add post and delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostsApiService } from './services/posts-api.service';
+import { BehaviorSubject } from 'rxjs';
+import { IPost } from './interfaces/post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-angular-test-app';
+export class AppComponent implements OnInit {
+  posts$ = new BehaviorSubject<IPost[] | null>(null);
+
+  constructor(private postsApiService: PostsApiService) { }
+
+  ngOnInit(): void {
+    this.loadPosts()
+  }
+
+  loadPosts() {
+    this.postsApiService.getPosts().subscribe(posts => this.posts$.next(posts));
+  }
+
+  addNew() {
+    const post = { title: (Math.random() * 10000).toString(), author: 'angular' }
+    this.postsApiService.postPost(post).subscribe(newPost => {
+      const oldPosts = this.posts$.getValue() ?? [];
+      this.posts$.next([...oldPosts, newPost]);
+    })
+  }
+
+  deletePost(id: string) {
+    this.postsApiService.deletePost(id).subscribe(() => {
+      const oldPosts = this.posts$.getValue() ?? [];
+      this.posts$.next(oldPosts.filter(post => post.id !== id));
+    })
+  }
 }
---
+++ src/app/services/posts-api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http"
+import {Observable} from 'rxjs';
+import { IPost } from '../interfaces/post';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsApiService {
+  private httpBase = 'http://localhost:3000/posts'
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<IPost[]> {
+    return this.http.get<IPost[]>(this.httpBase);
+  }
+  postPost(post: Omit<IPost, 'id'>): Observable<IPost> {
+    return this.http.post<IPost>(this.httpBase, post);
+  }
+  deletePost(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.httpBase}/${id}`)
+  }
+}
